d00d8c2865ac507dd9c4f4ddc422428d9e5d9bb4
Dev <devrathor989@gmail.com>
2026-02-26 09:29:50 +0530

    env setup

---
+++ intelliqon-website/src/app/page.js
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Intelliqon - Official Website</h1>
+    </main>
+  );
+}
